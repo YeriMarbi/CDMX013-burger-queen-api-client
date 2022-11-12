23c35b390868dc4d197c3f70170a0f2d50e87c08
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-11-11 18:46:58 -0600

    style login h1 and logo

--- my-app/src/login.jsx
+++ my-app/src/login.jsx
@@ -16,6 +16,7 @@ const Welcome = () => {
 
 const Logo = () => {
     return <div className='logo'>
+        <img src="https://user-images.githubusercontent.com/101679628/201446330-4a95197e-4311-4ec2-88d2-401155646188.png" alt="BurgerQueenLogo" />
         </div>
 };
 
