2c7c8dba3c70c8ea158167eaceccd864e22862b5
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-11-15 18:47:34 -0600

    table

--- my-app/src/components/AdminTable.jsx
+++ my-app/src/components/AdminTable.jsx
@@ -1,52 +1,101 @@
 import axios from 'axios'
 import * as React from 'react';
-import MUIDataTable from "mui-datatables"
+// import MUIDataTable from "mui-datatables"
 import { useState, useEffect } from 'react';
+import DataTable from 'react-data-table-component'
 // import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 
-export const TableBasic = () => {
-    const [people, setPeople] = useState([])
+export const Table = () => {
+    const [users, setUsers] = useState([])
+    const URL = 'https://637265f4025414c6370eb684.mockapi.io/api/bq/users'
 
-    const endpoint='https://637265f4025414c6370eb684.mockapi.io/api/bq/users'
-
-    const getData= async () => {
-        await axios.get(endpoint).then(result => {
+    const getData = async () => {
+        await axios.get(URL).then(result => {
             const data = result.data;
             console.log(data);
-            setPeople(data);
+            setUsers(data);
         }
-        )};
-        useEffect(()=>{
-            getData()
-        },[])
+        )
+    };
 
-        const columns=[
-            {
-                name:'name',
-                label:'NAME'
-            },
-            {
-                name:'area',
-                label:'AREA'
-            },
-            {
-                name:'e-mail',
-                label:'E-MAIL'
-            },
-            {
-                name:'password',
-                label:'PASSWORD'
-            },
-        ]
+    useEffect(() => {
+        getData()
+    }, []);
+
+    const columns = [
+        {
+            name: 'name',
+            selector: row => row.name
+        },
+        {
+            name: 'area',
+            selector: row => row.area
+        },
+        {
+            name: 'e-mail',
+            selector: row => row.email
+        },
+        {
+            name: 'password',
+            selector: row => row.password
+        },
+    ]
 
     return (
-        <MUIDataTable
-            title={'Lista de Empleados'}
-            data={people}
+        <div className='Table'>
+            <h1>HOLa</h1>
+            <DataTable
             columns={columns}
-        />
+            data={users}
+            />
+        </div>
     )
 }
+
+
+// export const TableBasic = () => {
+//     const [people, setPeople] = useState([])
+
+//     const endpoint='https://637265f4025414c6370eb684.mockapi.io/api/bq/users'
+
+//     const getData= async () => {
+//         await axios.get(endpoint).then(result => {
+//             const data = result.data;
+//             console.log(data);
+//             setPeople(data);
+//         }
+//         )};
+//         useEffect(()=>{
+//             getData()
+//         },[])
+
+//         const columns=[
+//             {
+//                 name:'name',
+//                 label:'NAME'
+//             },
+//             {
+//                 name:'area',
+//                 label:'AREA'
+//             },
+//             {
+//                 name:'e-mail',
+//                 label:'E-MAIL'
+//             },
+//             {
+//                 name:'password',
+//                 label:'PASSWORD'
+//             },
+//         ]
+
+//     return (
+//         <MUIDataTable
+//             title={'Lista de Empleados'}
+//             data={people}
+//             columns={columns}
+//         />
+//     )
+// }
 
 // const DataBQ = () => {
 //     axios.get('https://637265f4025414c6370eb684.mockapi.io/api/bq/users').then(result => {
@@ -83,4 +132,4 @@ export const TableBasic = () => {
 //     });
 // }
 
-export default TableBasic
+export default Table
