ae16212ef37744443d6226b901df886c0b10875f
yerimarbi <marbizepeda@gmail.com>
2022-11-22 11:10:06 -0600

    WIP style admin

--- my-app/src/components/Admin.jsx
+++ my-app/src/components/Admin.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import { Table } from './AdminTable';
+import "./admin.css";
+import Logo from './Logo.jsx'
+
+const ButtonsMenu = () => {
+    return (
+        <section className='menuBtn'>
+            <button id="emplBtn">EMPLEADOS</button><br></br>
+            <button id="prodBtn">PRODUCTOS</button>
+            <Logo color ='red' />
+            <button id="exit">SALIR</button>
+
+        </section>
+    )
+}
+
+const InputEmployee = () => {
+    return (
+        <section className='tabEmpl'>
+            <div className='employee'>
+                <input placeholder='Nombre' className='inputAdm'></input>
+                <input placeholder='Área' className='inputAdm'></input>
+                <input placeholder='Correo' className='inputAdm'></input>
+                <input placeholder='Contraseña' className='inputAdm'></input>
+                <button id="addEmployee">AGREGAR</button>
+            </div>
+            <Table></Table>
+        </section>
+    )
+}
 
 const Admin = () => {
     return (
-        <div>
-            <h1>Lista de productos</h1>
-            <Table></Table>
+        <div className='backAdmin'>
+            <ButtonsMenu />
+            <InputEmployee />
         </div>
     )
 }
