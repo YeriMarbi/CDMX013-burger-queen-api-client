9fd5e123dba02317e91d5b986214322c2ff2acda
DianaAmpudia <dianalauraampudia@gmail.com>
2022-11-24 14:08:25 -0600

    WIP styling modal

--- my-app/src/components/elements/Modal.jsx
+++ my-app/src/components/elements/Modal.jsx
@@ -5,8 +5,11 @@ return(
     <section className="modal">
     <div className="modalUser">
         <p>¿Deseas eliminar este usuario?</p>
+        <img src="https://media.tenor.com/erjWKt2RLw4AAAAM/jim-carrey-dumb-and-dumber.gif" alt="gifModal"/>
+        <div className='botones'>
         <button className="agreeBtn" onClick={funBorrar}>Aceptar</button>
         <button className="cancelBtn" onClick={funCerrar}>Cancelar</button>
+        </div>
     </div>
     </section>
 )
